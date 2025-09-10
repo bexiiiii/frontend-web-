a0a15a21163e065a02f27f0e1d235bb3daa9627d
Dynamite01 <behruzbektohtamishov@gmail.com>
2025-09-11 01:03:41 +0500

    analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { NavBarComponent } from "@/components/NavbarComponent";
 
+
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
